f70af8dffe9c317c07921d0511fb2b2210ea6854
Vloukk <v.valentin.evangelista@gmail.com>
2024-02-21 18:38:54 +0100

    profil pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import './styles/App.scss';
 
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
+import Profil from './pages/profil/Profil';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/profil" element={<Profil />} />
       </Routes>
     </Router>
   );
---
+++ front-end/src/pages/profil/Profil.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/header/Header';
+
+function Profil() {
+
+  return (
+    <div className='profil'>
+        <Header />
+    </div>
+  );
+}
+
+export default Profil;
