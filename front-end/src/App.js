01b84539a80fdc5d233351490470ded19d5cfa45
Vloukk <v.valentin.evangelista@gmail.com>
2024-03-06 19:19:57 +0100

    user protect

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,7 +3,7 @@ import './styles/App.scss';
 
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
-import Profil from './pages/profil/Profil';
+import Profil from './pages/profil/user/Profil';
 
 function App() {
   return (
